a12a02405593e104c61567efce9c7f51591d7489
asitranjan23 <124146030+asitranjan23@users.noreply.github.com>
2025-01-08 15:36:01 +0000

    string combination

---
+++ .devcontainer/Basics/03basics.js
@@ -0,0 +1,20 @@
+let number=2324
+let stringNo=String(number)
+console.log(number);
+// console.log(typeof stringNo);
+
+// console.log(typeof number);
+
+// console.log(stringNo);
+let value=3
+let negValue=-value
+console.log(negValue);
+console.log(-4);
+console.log(2**5);
+let str1="hello"
+let str2=" Asit"
+let str3=str1+str2
+console.log(str3);
+console.log("a"+b);
+
+
