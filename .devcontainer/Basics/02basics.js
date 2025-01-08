9ecaa974382c3d70e9d32437aa2df6f6e03008d1
asitranjan23 <124146030+asitranjan23@users.noreply.github.com>
2025-01-08 15:18:17 +0000

    datatype conversion

---
+++ .devcontainer/Basics/02basics.js
@@ -0,0 +1,41 @@
+console.log("another day")
+const acId=123213
+let accountEmail="ranjaan@gmail.com"
+let jaipur;
+console.log(accountEmail)
+console.table([acId,accountEmail,accountEmail]);
+{
+    let x=20;
+    var y=30;
+}
+// console.log(x);
+console.log(y);
+console.log(typeof jaipur);
+console.log(3+9);
+// let String x;
+// console.log(typeof x);
+console.log(typeof null);
+let Score="3t3"
+let ant=true
+let ant2=""
+let ant3="Asit"
+let converted= Number(Score)
+console.log(typeof converted);
+console.log(converted);
+let tna=Number(ant)
+let tna2=Number(ant2)
+console.log(tna);
+console.log(tna2);
+let tna3=Number(ant3)
+console.log(tna3);
+
+
+
+
+
+
+
+
+
+
+
